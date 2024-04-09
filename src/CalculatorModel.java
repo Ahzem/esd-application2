7996dc5ee920058ce6ebe27de818dc4e15c6a51d
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-04-09 11:50:08 +0530

    All are updated

---
+++ src/CalculatorModel.java
@@ -0,0 +1,11 @@
+public class CalculatorModel {
+    private int answer;
+
+    public void addTwoNumbers(int firstNumber, int secondNumber) {
+        answer = firstNumber + secondNumber;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+}
